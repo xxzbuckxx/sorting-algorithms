e13a8bb14b80c4a596a19bdcfc94714e28a53ebb
Zack Traczyk <cookies951@gmail.com>
2021-03-29 16:28:40 -0500

    asgn1 init

---
+++ asgn1/README.md
@@ -0,0 +1,9 @@
+# Assignment 1 - Left, Right and Center
+
+Play a game called left, right and center.
+
+# How to Build
+
+# Any Problems
+
+# File Explanation
---
+++ asgn1/lrc.c
@@ -0,0 +1,22 @@
+
+typedef enum faciem {PASS, LEFT, RIGHT, CENTER} faces;
+faces die [] = {LEFT, RIGHT, CENTER, PASS, PASS, PASS};
+
+char * philosophers [] = {" Immanuel Kant ",
+                          "Martin Heidegger",
+                          "David Hume",
+                          "Georg Wilhelm Friedrich Hegel",
+                          "Arthur Schopenhauer",
+                          "Ludwig Wittgenstein",
+                          "Karl Wilhelm Friedrich Schlegel",
+                          "Friedrich Nietzsche",
+                          "Socrates",
+                          "John Stuart Mill",
+                          "Plato",
+                          "Aristotl",
+                          "Thomas Hobbes",
+                          "Rene Descartes"};
+
+int main() {
+    return 0;
+}
