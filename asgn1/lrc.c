52886619641b180d99f44f4fbb868aaa5a7ff321
Zack Traczyk <ztraczyk@ucsc.edu>
2021-03-30 17:17:32 -0500

    asn1 helper functions added

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -1,22 +1,48 @@
+#include <stdio.h>
 
 typedef enum faciem {PASS, LEFT, RIGHT, CENTER} faces;
 faces die [] = {LEFT, RIGHT, CENTER, PASS, PASS, PASS};
 
-char * philosophers [] = {" Immanuel Kant ",
-                          "Martin Heidegger",
-                          "David Hume",
-                          "Georg Wilhelm Friedrich Hegel",
-                          "Arthur Schopenhauer",
-                          "Ludwig Wittgenstein",
-                          "Karl Wilhelm Friedrich Schlegel",
-                          "Friedrich Nietzsche",
-                          "Socrates",
-                          "John Stuart Mill",
-                          "Plato",
-                          "Aristotl",
-                          "Thomas Hobbes",
-                          "Rene Descartes"};
+char *philosophers[] = {"Immanuel Kant ",
+                        "Martin Heidegger",
+                        "David Hume",
+                        "Georg Wilhelm Friedrich Hegel",
+                        "Arthur Schopenhauer",
+                        "Ludwig Wittgenstein",
+                        "Karl Wilhelm Friedrich Schlegel",
+                        "Friedrich Nietzsche",
+                        "Socrates",
+                        "John Stuart Mill",
+                        "Plato",
+                        "Aristotle",
+                        "Thomas Hobbes",
+                        "Rene Descartes"};
+
+uint8_t left(uint8_t pos, uint8_t players);
+uint8_t right(uint8_t pos, uint8_t players);
 
 int main() {
     return 0;
+}
+
+/* Functions left and right provided by Darrel Long */
+
+/*
+* Returns the position of the player to the left.
+*
+* pos:     The position of the current player.
+* players: The number of players in the game.
+*/
+static inline uint8_t left(uint8_t pos, uint8_t players) {
+	return ((pos + players -1) % players);
+}
+
+/*
+* Returns the position of the player to the right.
+*
+* pos:     The position of the current player.
+* players: The number of players in the game.
+*/
+static inline uint8_t right(uint8_t pos, uint8_t players) {
+	return ((pos + 1) % players);
 }
