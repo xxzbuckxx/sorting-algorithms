f46d56b828822c2c61e9e9705123ed4a6085eee3
Zack Traczyk <ztraczyk@ucsc.edu>
2021-04-07 00:31:01 -0500

    asgn2 getopt structure

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -1,7 +1,32 @@
+#include <stdio.h>
+#include <unistd.h> // For getopt()
 
+#define OPTIONS "asctl"
 
-int main() {
+int main(int argc, char **argv) {
+    int opt = 0;
+    
+    // Remember to ensure that tests do not run twice
 
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'a':
+            printf("-a option.\n");
+            break;
+        case 's':
+            printf("-s option.\n");
+            break;
+        case 'c':
+            printf("-c option.\n");
+            break;
+        case 't':
+            printf("-t option.\n");
+            break;
+        case 'l':
+            printf("-l option.\n");
+            break;
+        }
+    }
     // printf(" %7.41f % 16.81f % 16.81f % 16.101f\n", ...);
     return 0;
 }
