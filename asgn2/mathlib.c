b8927a405b911546b6f48474b46f444e43a0123f
Zack Traczyk <ztraczyk@ucsc.edu>
2021-04-06 23:27:54 -0500

    asgn2 started

---
+++ asgn2/mathlib.c
@@ -0,0 +1,21 @@
+#include "mathlib.h"
+
+double arcSin(double x) {
+    // Implement
+    return x;
+}
+
+double arcCos(double x) {
+    // Implement
+    return x;
+}
+
+double arcTan(double x) {
+    // Implement
+    return x;
+}
+
+double Log(double x) {
+    // Implement
+    return x;
+}
