61e8cd5cc74d46b200249dfd087e09e437766ec5
Zack Traczyk <ztraczyk@ucsc.edu>
2021-04-18 16:50:21 -0500

    asgn2 graphs enlarged

--- notes/asgn2 writeup/plot.sh
+++ notes/asgn2 writeup/plot.sh
@@ -12,15 +12,17 @@ do
 done
 make clean && cd ../notes/asgn2\ writeup
 gnuplot <<XX
-set terminal jpeg
-set output "arcsin.jpg"
-set pointsize 0.1
-plot "/tmp/asin.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's arcsin(x)"
-set output "arccos.jpg"
-plot "/tmp/acos.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's arccos(x)"
+set terminal jpeg size 1200, 800
+set pointsize 0.2
 set output "arctan.jpg"
-plot "/tmp/atan.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's arctan(x)"
+plot "/tmp/atan.dat" with points lc rgbcolor "#724CEF" title "diff. btwn. std. lib and this lib's arctan(x)"
 set output "exp.jpg"
-plot "/tmp/log.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's log(x)"
+plot "/tmp/log.dat" with points lc rgbcolor "#724CEF" title "diff. btwn. std. lib and this lib's log(x)"
+set arrow from -0.7, graph 0 to -0.7, graph 1 nohead lc "#55bbbbbb"
+set arrow from 0.7, graph 0 to 0.7, graph 1 nohead lc "#55bbbbbb"
+set output "arcsin.jpg"
+plot "/tmp/asin.dat" with points lc rgbcolor "#724cEF" title "diff. btwn. std. lib and this lib's arcsin(x)"
+set output "arccos.jpg"
+plot "/tmp/acos.dat" with points lc rgbcolor "#724cEF" title "diff. btwn. std. lib and this lib's arccos(x)"
 XX
 
