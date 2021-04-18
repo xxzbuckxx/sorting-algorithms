13f404cf72e544a3d58e794a6a1f91eae0889a50
Zack Traczyk <ztraczyk@ucsc.edu>
2021-04-18 16:20:37 -0500

    asgn2 graphs updated

--- notes/asgn2 writeup/plot.sh
+++ notes/asgn2 writeup/plot.sh
@@ -10,16 +10,17 @@ do
 		./mathlib-test -${i:0:1} > /tmp/$i.dat
 	fi
 done
-cd ../notes/asgn2\ writeup
+make clean && cd ../notes/asgn2\ writeup
 gnuplot <<XX
 set terminal jpeg
 set output "arcsin.jpg"
-plot "/tmp/asin.dat" with linespoints title "diff. btwn. std. lib and this lib's arcsin(x)"
+set pointsize 0.1
+plot "/tmp/asin.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's arcsin(x)"
 set output "arccos.jpg"
-plot "/tmp/acos.dat" with linespoints title "diff. btwn. std. lib and this lib's arccos(x)"
+plot "/tmp/acos.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's arccos(x)"
 set output "arctan.jpg"
-plot "/tmp/atan.dat" with linespoints title "diff. btwn. std. lib and this lib's arctan(x)"
+plot "/tmp/atan.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's arctan(x)"
 set output "exp.jpg"
-plot "/tmp/log.dat" with linespoints title "diff. btwn. std. lib and this lib's log(x)"
+plot "/tmp/log.dat" with points lc rgbcolor "blue" title "diff. btwn. std. lib and this lib's log(x)"
 XX
 
