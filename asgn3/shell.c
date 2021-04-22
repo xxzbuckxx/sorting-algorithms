3a1dadba8c6d3b68a3ddfea9d37efe8afd4a8ebe
Zack Traczyk <ztraczyk@ucsc.edu>
2021-04-21 23:29:21 -0500

    asgn3 shell sort fin

--- asgn3/shell.c
+++ asgn3/shell.c
@@ -1,10 +1,28 @@
 #include "shell.h"
+#include "gaps.h"
 #include "analytics.h"
 
 #include <stdio.h>
 
 void shell_sort(uint32_t *A, uint32_t n) {
-    // Does the thing
-    A[0] = A[0];
-    n = 1 + 1;
+    
+    for (uint32_t i = 0; i < GAPS; i++) {
+
+        // Skip if gap bigger than array size
+        if (gaps[i] > n) {
+            continue;
+        }
+
+        for (uint32_t j = gaps[i]; j < n; j++){
+            uint32_t t = j; // Store j since it will be subtracted
+            uint32_t temp = A[j];
+            comparisons++;
+            while (t >= gaps[i] && temp < A[t - gaps[i]]) {
+                A[t] = A[t - gaps[i]];
+                moves++;
+                t -= gaps[i];
+            }
+            A[t] = temp;
+        }
+    }
 }
